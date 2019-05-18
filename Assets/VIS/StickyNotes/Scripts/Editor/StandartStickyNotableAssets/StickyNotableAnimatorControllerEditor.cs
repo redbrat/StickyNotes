4cefbddb841e7842a3108965cd12cee6f4f42d54
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2019-05-19 01:49:19 +0300

    AnimatorController stickynotable editor implemented

--- Assets/VIS/StickyNotes/Scripts/Editor/StandartStickyNotableAssets/StickyNotableAnimatorControllerEditor.cs
+++ Assets/VIS/StickyNotes/Scripts/Editor/StandartStickyNotableAssets/StickyNotableAnimatorControllerEditor.cs
@@ -1,81 +1,81 @@
 ﻿using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
+using VIS.ObjectDescription.Editor;
 using VIS.ObjectDescription.ScriptableObjects;
 
-namespace VIS.ObjectDescription.Editor.StandartStickyNotableAssets
+[CustomEditor(typeof(AnimatorController))]
+public class StickyNotableAnimatorControllerEditor : UnityEditor.Editor, IAssetsStickedEventsListener
 {
-    //[CustomEditor(typeof(AnimatorController))]
-    public class StickyNotableAnimatorControllerEditor : UnityEditor.Editor, IAssetsStickedEventsListener
+    private GenericStickyNoteEditorBehaviour _stickyNoteEditorBehaviour
     {
-        private GenericStickyNoteEditorBehaviour _stickyNoteEditorBehaviour
+        get
         {
-            get
-            {
-                if (_stickyNoteEditorBehaviourBackingField == null)
-                    _stickyNoteEditorBehaviourBackingField = new GenericStickyNoteEditorBehaviour(
-                        base.OnInspectorGUI,
-                        findProperty,
-                        applyModifiedProperties,
-                        () => needToDrawBaseInspector
-                    );
+            if (_stickyNoteEditorBehaviourBackingField == null)
+                _stickyNoteEditorBehaviourBackingField = new GenericStickyNoteEditorBehaviour(
+                    base.OnInspectorGUI,
+                    findProperty,
+                    applyModifiedProperties,
+                    () => needToDrawBaseInspector
+                );
 
-                return _stickyNoteEditorBehaviourBackingField;
-            }
+            return _stickyNoteEditorBehaviourBackingField;
         }
-        private GenericStickyNoteEditorBehaviour _stickyNoteEditorBehaviourBackingField;
+    }
+    private GenericStickyNoteEditorBehaviour _stickyNoteEditorBehaviourBackingField;
 
-        private SerializedObject _targetCache;
+    private SerializedObject _targetCache;
 
-        public void OnEnable()
-        {
-            if (_targetCache == null)
-                setRightTarget();
+    public void OnEnable()
+    {
+        if (_targetCache == null)
+            setRightTarget();
 
-            if (_targetCache != null)
-                _stickyNoteEditorBehaviour.OnEnable();
-        }
+        //Debug.Log($"Material OnEnable. _targetCache = {_targetCache}");
+        if (_targetCache != null)
+            _stickyNoteEditorBehaviour.OnEnable();
+    }
 
-        public void OnDisable()
-        {
-            if (_targetCache != null)
-                _stickyNoteEditorBehaviour.OnDisable();
-        }
+    public void OnDisable()
+    {
+        if (_targetCache != null)
+            _stickyNoteEditorBehaviour.OnDisable();
+    }
 
-        public override void OnInspectorGUI()
-        {
-            if (_targetCache == null)
-                base.OnInspectorGUI();
-            else
-                _stickyNoteEditorBehaviour.OnInspectorGUI();
-        }
+    public override void OnInspectorGUI()
+    {
+        //Debug.Log($"Material OnInspectorGUI. _targetCache = {_targetCache}");
+        if (_targetCache == null)
+            base.OnInspectorGUI();
+        else
+            _stickyNoteEditorBehaviour.OnInspectorGUI();
+    }
 
-        private SerializedProperty findProperty(string propertyName)
-        {
-            return _targetCache.FindProperty(propertyName);
-        }
+    private SerializedProperty findProperty(string propertyName)
+    {
+        return _targetCache.FindProperty(propertyName);
+    }
 
-        private void applyModifiedProperties()
-        {
-            _targetCache.ApplyModifiedProperties();
-        }
+    private void applyModifiedProperties()
+    {
+        _targetCache.ApplyModifiedProperties();
+    }
 
-        private bool needToDrawBaseInspector => true;
+    private bool needToDrawBaseInspector => true;
 
-        private void setRightTarget()
-        {
-            var assetPath = AssetDatabase.GetAssetPath(target);
-            var asset = AssetDatabase.LoadAssetAtPath<StickyNote>(assetPath);
-            if (asset != null)
-                _targetCache = new SerializedObject(asset);
-        }
+    private void setRightTarget()
+    {
+        var assetPath = AssetDatabase.GetAssetPath(target);
+        var asset = AssetDatabase.LoadAssetAtPath<StickyNote>(assetPath);
+        if (asset != null)
+            _targetCache = new SerializedObject(asset);
+    }
 
-        public void OnSticked() => OnEnable();
+    public void OnSticked() => OnEnable();
 
-        public void OnUnsticked()
-        {
-            _targetCache = null;
-            OnEnable();
-        }
+    public void OnUnsticked()
+    {
+        _targetCache = null;
+        OnEnable();
     }
 }
