c51e626e4b28f165fe220c02c876bdb5879cc8a9
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2019-05-20 02:02:47 +0300

    icon somewhat added

--- Assets/VIS/StickyNotes/Scripts/ScriptableObjects/Editor/StickyNoteEditor.cs
+++ Assets/VIS/StickyNotes/Scripts/ScriptableObjects/Editor/StickyNoteEditor.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEditor;
+using UnityEngine;
 using VIS.ObjectDescription.Editor;
 
 namespace VIS.ObjectDescription.ScriptableObjects.Editor
 {
     [CustomEditor(typeof(StickyNote))]
-    public sealed class StickyNoteEditor : StickyNoteEditorBase { }
+    public sealed class StickyNoteEditor : StickyNoteEditorBase
+    {
+        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+        {
+            var tex = Resources.Load<Texture2D>("VIS/StickyNotes/Textures/note-icon");
+            Debug.Log($"tex = {tex}. {width}, {height}");
+            //tex.SetPixel(0, 0, Color.yellow);
+            //tex.Apply();
+            return tex;
+        }
+    }
 }
